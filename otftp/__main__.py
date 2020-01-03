a42b1fbd0c8ef552df4876f11b7e76ae17461aee
Andrea Peter <andrea.peter@bluebotics.com>
2020-01-03 10:40:31 +0100

    Workaround for asyncio and SIGINT issue
    On Windows, with python version < 3.8.1,
    SIGINT does not work as expected in the asyncio event loop,
    see: https://bugs.python.org/issue23057
    
    This is a workaround

--- otftp/__main__.py
+++ otftp/__main__.py
@@ -17,6 +17,16 @@ def main():
     }
     loop = asyncio.get_event_loop()
 
+    def _wakeup():
+        """
+        This method is used to wake up the loop at regular intervals,
+        this is a workaround for the SIGINT issue: https://bugs.python.org/issue23057
+        (affects python version < 3.8.1 on Windows)
+        """
+        loop.call_later(0.1, _wakeup)
+
+    loop.call_later(0.1, _wakeup)
+
     listen = loop.create_datagram_endpoint(
         lambda: OberonTFTPServerProtocol(args.files_dir, args.host, loop, timeouts),
         local_addr=(args.host, args.port,))
